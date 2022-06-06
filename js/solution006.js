6bd929c3e23a79891c88e00deb893559fbeea938
CrimpyCode <wsung88@gmail.com>
2022-06-06 15:09:45 -0500

    codewars 7kyu Array Array Array was complete

--- js/solution006.js
+++ js/solution006.js
@@ -1,5 +1,23 @@
-function span(arr, predicate) {
-    let newArr = []
-    arr.map((x)=> {predicate(x)?newArr[0].push():newArr[1].push()})
-    return newArr
+function explode(x){
+  let score = 0
+  let arr = []
+  if (Number(x[0])===x[0]&&Number(x[1])===x[1]){
+    score += x[0] + x[1]}
+
+
+  else if (Number(x[0])===x[0]&&Number(x[1])!==x[1]){
+    score += x[0]}
+
+    else if (Number(x[1])===x[1]&&Number(x[0])!==x[0]){
+    score += x[1]}
+
+  else return 'Void!'
+
+  for (let i = 1; i <= score; i++) {
+    arr.push(x)
   }
+  
+  return arr
+}
+
+explode([5,4])
