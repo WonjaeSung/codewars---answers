0a24a147d474a6659cd0d13257a9ae46e2268407
CrimpyCode <wsung88@gmail.com>
2022-06-21 15:32:25 -0500

    codewars 8kyu Removing Elements complete

--- js/solution036.js
+++ js/solution036.js
@@ -1,7 +1,20 @@
-String.prototype.toJadenCase = function () {
-    let arr = this.split(' ')
-    for(let i = 0; i < arr.length; i++){
-        arr[i] = arr[i][0].toUpperCase()+ arr[i].substr(1);
-    }
-    return arr.join(' ')
-  };
+function solution(number){
+    let multiOfThree = 3
+    let multiOfFive = 5
+    let i = 0
+    let j = 0
+    let answer = 0
+    while(multiOfThree<number){
+      i++;
+      answer += multiOfThree
+      multiOfThree += 3
+      console.log(answer)
+      }
+    while(multiOfFive<number){
+      j++;
+      answer += multiOfFive
+      multiOfFive += 5
+       console.log(answer)
+      }
+    return answer
+  }
---
+++ js/solution037.js
@@ -0,0 +1,8 @@
+function removeEveryOther(arr){
+    let newArr=[]
+    for (let i=0; i<arr.length; i++)
+      {newArr.push(arr[i]);
+       i++
+      }
+      console.log(newArr)
+  }
