4aa44a8035566be88093feb60dfeeaa440eaad36
CrimpyCode <wsung88@gmail.com>
2022-06-26 20:12:20 -0500

    codewars 7kyu You're a square! complete commiting on previous date

---
+++ js/solution042.js
@@ -0,0 +1,7 @@
+var isSquare = function(n){
+    //console.log(Math.floor(Math.sqrt(n)))
+    
+    //console.log(Math.ceil(Math.sqrt(n)))
+
+    return Math.floor(Math.sqrt(n))===Math.ceil(Math.sqrt(n))
+  }
